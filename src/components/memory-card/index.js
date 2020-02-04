4b325372b99d97fa4b77895fef4fb1a59f0230d0
RafaelPiedade <rafaeldade@hotmail.com>
2020-02-04 16:54:31 -0200

    :white_check_mark: Aula 023

---
+++ src/components/memory-card/index.js
@@ -0,0 +1,33 @@
+//const $wrapCards = document.querySelector(".wrap-cards");
+function createMemoryCard() {
+  const $memoryCard = document.createElement("article");
+  
+  const $iconCollab = `
+    <img 
+    class='icon'
+    src='img/icon-collabcode.png'
+    alt='Gueio mascote da CollabCode'
+    />`;
+
+  $memoryCard.classList.add("memory-card");
+  $wrapCards.insertBefore($memoryCard, null);
+  
+  $memoryCard.insertAdjacentHTML("afterbegin", $iconCollab);
+}
+
+
+function createMemoryCardFront(){
+  const $memoryCard = document.createElement("article");
+  
+  const $iconCollab = `
+    <img 
+    class='icon'
+    src='img/icon-c.png'
+    alt='Gueio mascote da CollabCode'
+    />`;
+
+  $memoryCard.classList.add("memory-card", "-front");
+  $wrapCards.insertBefore($memoryCard, null);
+  
+  $memoryCard.insertAdjacentHTML("afterbegin", $iconCollab);
+}
