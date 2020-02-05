11125442347c2a7a559f87c0b219ee517bf3b3a1
rafaelpiedade <rafaeldade@hotmail.com>
2020-02-05 08:20:31 -0200

    :white_check_mark: Aula 024

--- src/components/memory-card/index.js
+++ src/components/memory-card/index.js
@@ -1,31 +1,40 @@
-//const $wrapCards = document.querySelector(".wrap-cards");
 function createMemoryCard() {
-  const $memoryCard = document.createElement("article");
-
-  const $iconCollab = `
+  /*const $memoryCard = document.createElement("article");
+  $memoryCard.classList.add("memory-card"); */
+  const $memoryCard = `
+  <article class="memory-card">
     <img 
-    class='icon'
-    src='img/icon-collabcode.png'
-    alt='Gueio mascote da CollabCode'
-    />`;
-
-  $memoryCard.classList.add("memory-card");
-  $wrapCards.insertBefore($memoryCard, null);
-  $memoryCard.insertAdjacentHTML("afterbegin", $iconCollab);
+      class='icon'
+      src='img/icon-collabcode.png'
+      alt='Gueio mascote da CollabCode'
+      onClick="handleClick()"
+      />
+  </article>
+  `;
+  return $memoryCard;
 }
 
 function createMemoryCardFront() {
-  const $memoryCard = document.createElement("article");
-
-  const $iconCollab = `
+  const $memoryCard = `
+  <article class="memory-card -front">
     <img 
-    class='icon'
-    src='img/icon-c.png'
-    alt='Gueio mascote da CollabCode'
-    />`;
-
-  $memoryCard.classList.add("memory-card", "-front");
-  $wrapCards.insertBefore($memoryCard, null);
-
-  $memoryCard.insertAdjacentHTML("afterbegin", $iconCollab);
+      class='icon'
+      src='img/icon-c.png'
+      alt='Gueio mascote da CollabCode'
+      onClick="handleClick()"
+      />
+  </article>
+  `;
+  return $memoryCard;
 }
+
+function handleClick() {
+  console.log('ee')
+}
+
+/*
+cria elemento > const $memoryCard = document.createElement("article");
+add classes   > $memoryCard.classList.add("memory-card", "-front");
+insere na tela >$wrapCards.insertBefore($memoryCard, null); não é do componente
+insere o icon >$memoryCard.insertAdjacentHTML("afterbegin", $iconCollab);
+*/
---
+++ src/objects/cards-wrapper/index.js
@@ -0,0 +1,12 @@
+function createCardsWrapper() {
+  const $cardsWrapper = document.createElement("section");
+  $cardsWrapper.classList.add("cards-wrapper");
+
+  return $cardsWrapper;
+}
+
+/* $cardsWrapper.addEventListener("click", e => {
+  if (e.target.className == "memory-card" || e.target.className == "icon") {
+    console.log("ae");
+  }
+}); */
